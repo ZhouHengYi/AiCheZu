4af761dcd23684c97571967dc443ea2a48f442a0
Henry.Zhou <zhouhengyi@rrliuxue.com>
2015-09-12 09:23:19 +0800

    2

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,43 @@
+Template.postSubmit.events({
+	'submit form': function(e) {
+		e.preventDefault();
+
+		var post = {
+			url: $(e.target).find('[name=url]').val(),
+			title: $(e.target).find('[name=title]').val()
+		};
+
+			
+	    var errors = validatePost(post);
+	    if (errors.title || errors.url)
+	      return Session.set('postSubmitErrors', errors);
+		
+		//post._id = Posts.insert(post);
+		//Router.go('postPage', post);
+		Meteor.call('postInsert', post, function(error, result) {
+			// 显示错误信息并退出
+			if (error)
+				return alert(error.reason);
+
+			// 显示结果，跳转页面
+			if (result.postExists)
+				alert('This link has already been posted（该链接已经存在）');
+
+			Router.go('postPage', {
+				_id: result._id
+			});
+		});
+	}
+});
+
+Template.postSubmit.onCreated(function() {
+  Session.set('postSubmitErrors', {});
+});
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
